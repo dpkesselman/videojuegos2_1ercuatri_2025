3f5ae478d38498acb1abc2a2d2eb9c3be7339bb4
Daniela Kesselman <d.p.kesselman@gmail.com>
2025-04-10 19:51:10 -0300

    animaciones

--- Assets/Scripts/Movimiento/PlayerMovement3.cs
+++ Assets/Scripts/Movimiento/PlayerMovement3.cs
@@ -8,25 +8,51 @@ public class PlayerMovement3 : MonoBehaviour
   [SerializeField] private float speed = 3f;
   private Rigidbody2D rb;
   private Vector2 moveInput;
-
+  private Animator animator;
+  //private SpriteRenderer renderer;
+  private bool facingRight = true;
+  float moveX;
+  float moveY;
 
   void Start()
   {
     rb = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+    //renderer = GetComponent<SpriteRenderer>();
   }
 
 
   void Update()
   {
-    float moveX = Input.GetAxis("Horizontal");
-    float moveY = Input.GetAxis("Vertical");
+    moveX = Input.GetAxis("Horizontal");
+    moveY = Input.GetAxis("Vertical");
     moveInput = new Vector2(moveX, moveY).normalized;
+    rb.MovePosition(rb.position + moveInput * speed * Time.deltaTime);
+    Flip();
+    animator.SetFloat("moveX", Mathf.Abs(moveX));
+    animator.SetFloat("moveY", Mathf.Abs(moveY));
   }
 
-
-  void FixedUpdate()
+  /*private void Flip() // Usando el flip del renderer
   {
-    rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+    if (moveX < 0)
+    {
+      renderer.flipX = true;
+    }
+    else if (moveX > 0)
+    {
+      renderer.flipX = false;
+    }
+  }*/
+
+  private void Flip() // usando rotación del asset
+  {
+    if (moveX < 0 && facingRight || moveX > 0 && !facingRight)
+    {
+      facingRight = !facingRight;
+      transform.Rotate(new Vector3(0,180,0));
+    }
   }
+
 }
 
--- Assets/Scripts/Salto/Jump2.cs
+++ Assets/Scripts/Salto/Jump2.cs
@@ -8,17 +8,26 @@ public class Jump2 : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float JumpForce = 8f;
 
+    //Animation
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update ()
     {
         float h = Input.GetAxis("Horizontal");
 
 
         rb.transform.Translate(new Vector2(h, 0) * Time.deltaTime * speed);
+        animator.SetFloat("moveX", Mathf.Abs(h));
        
         if (Input.GetKeyDown(KeyCode.Space) && JumpCollider.isGrounded == true)
         {
             rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-        }
+        }        
     }
 }
 
